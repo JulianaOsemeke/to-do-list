598e07d913065e4e81c1d6abebef2ba9bf0a84d9
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-07-15 14:03:38 +0100

    Import  functions from logic.js files

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,5 @@
+import {saveLocalTodos, deleteMarker, filterTodo} from './logic.js'
+
 //Selectors
 const todoInput = document.querySelector(".input-todo");
 const todoButton = document.querySelector(".buttonlist");
@@ -38,80 +40,6 @@ function addTodo(event) {
    todoList.appendChild(todoDiv);
    //CLEAR todo input value
    todoInput.value ="";
-
-   
-  
 }
 
-function deleteMarker(e) {
-  const item = e.target;
-
-  //DELETE TODO
-  if(item.classList[0] === "delete-btn") {
-    const todo = item.parentElement;
-    //ANIMATION
-    todo.classList.add("fall");
-    todo.addEventListener('tarnsitionend',function() {
-      todo.remove();
-    });
-
-  }
-
-  //CHECK MARK
-  if (item.classList[0] === "complete-btn" ){
-    const todo = item.parentElement;
-    todo.classList.toggle("completed");
-  }
-}
-
-function filterTodo(e) {
-  const todos = todoList.childNodes;
-  todos.forEach(function(todo){
-    switch(e.target.value) {
-      case "all":
-        todo.style.display = "flex";
-        break;
-        case "completed":
-          if(todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-        case "uncompleted":
-          if(!todo.classList.contains("completed")) {
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-    }
-    
-
-  });
-}
-
-function saveLocalTodos(todo){
-  //cHECK ---HEY Do I already have thing in there?
-  let todos;
-  if(localStorage.getItem('todos') === null ){
-    todos = [];
-  } else{
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-function getTods(){
-  let todos;
-  if(localStorage.getItem('todos') === null ){
-    todos = [];
-  } else{
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-export default todo;
+export default addTodo;
